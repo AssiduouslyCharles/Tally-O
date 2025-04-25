ff355ae3a4060ad7c35b8333158cdedb470dca32
Charles <AssiduouslyCharles@gmail.com>
2025-04-24 22:08:53 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,9 @@ CREATE TABLE IF NOT EXISTS transactions (
 
 -- Create the sold_items table.
 -- Using order_id as the PRIMARY KEY (ensure that order_id is unique per sold item).
-CREATE TABLE IF NOT EXISTS sold_items (
+DROP TABLE IF EXISTS sold_items;
+
+CREATE TABLE sold_items (
     order_id TEXT,
     transaction_id TEXT,
     item_id TEXT,
